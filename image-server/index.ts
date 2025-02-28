66e5558346a6cfcdbd0022c5d20417125e70f8e7
olijwood <olijwood@gmail.com>
2025-02-28 18:56:21 +0000

    Try make image server work again

--- image-server/index.ts
+++ image-server/index.ts
@@ -1,11 +1,9 @@
-import path from "path";
-import { cwd } from "process";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 4001;
 
-app.use("/assets", express.static(path.join(cwd(), "assets")));
+app.use("/assets", express.static("assets"));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
