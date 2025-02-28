63526651b5927990b28a693886ac584339287ab8
olijwood <olijwood@gmail.com>
2025-02-28 20:10:47 +0000

    fix static assets path

--- image-server/index.ts
+++ image-server/index.ts
@@ -1,14 +1,9 @@
-import path from "path";
-import { fileURLToPath } from "url";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 4001;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use("/assets", express.static("assets"));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
