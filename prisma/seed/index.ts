e0e001a0c3eda886431ee209d0f458a33a050c84
olijwood <olijwood@gmail.com>
2025-03-14 19:41:07 +0000

    skip post seeding if post

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -30,80 +30,85 @@ async function main() {
     );
     console.log("✅ Users seeded successfully.");
 
-    console.log("🌱 Seeding posts, comments, reactions, and replies...");
-    for (const post of POSTS_DATA) {
-        const createdPost = await prisma.post.create({
-            data: {
-                id: post.id,
-                title: post.title,
-                slug: post.slug,
-                content: post.content,
-                published: post.published,
-                userId: post.userId,
-                bannerImgUrl: post.bannerImgUrl,
-            },
-        });
-
-        for (const tagName of post.tags) {
-            const tag = await prisma.tag.upsert({
-                where: { name: tagName },
-                update: {},
-                create: {
-                    name: tagName,
-                    description: "",
-                    color: "text-gray-600",
+    const isPost = await prisma.post.findFirst({ select: { id: true } });
+    if (isPost) {
+        console.log("🌱 Posts already exist, skipping post seeding.");
+    } else {
+        console.log("🌱 Seeding posts, comments, reactions, and replies...");
+        for (const post of POSTS_DATA) {
+            const createdPost = await prisma.post.create({
+                data: {
+                    id: post.id,
+                    title: post.title,
+                    slug: post.slug,
+                    content: post.content,
+                    published: post.published,
+                    userId: post.userId,
+                    bannerImgUrl: post.bannerImgUrl,
                 },
             });
 
-            await prisma.postTag.upsert({
-                where: {
-                    postId_tagId: {
+            for (const tagName of post.tags) {
+                const tag = await prisma.tag.upsert({
+                    where: { name: tagName },
+                    update: {},
+                    create: {
+                        name: tagName,
+                        description: "",
+                        color: "text-gray-600",
+                    },
+                });
+
+                await prisma.postTag.upsert({
+                    where: {
+                        postId_tagId: {
+                            postId: createdPost.id,
+                            tagId: tag.id,
+                        },
+                    },
+                    update: {},
+                    create: {
                         postId: createdPost.id,
                         tagId: tag.id,
                     },
-                },
-                update: {},
-                create: {
-                    postId: createdPost.id,
-                    tagId: tag.id,
-                },
-            });
-        }
+                });
+            }
 
-        for (const comment of post.comments) {
-            const createdComment = await prisma.comment.create({
-                data: {
-                    id: comment.id,
-                    content: comment.content,
-                    userId: comment.userId,
-                    postId: createdPost.id,
-                },
-            });
+            for (const comment of post.comments) {
+                const createdComment = await prisma.comment.create({
+                    data: {
+                        id: comment.id,
+                        content: comment.content,
+                        userId: comment.userId,
+                        postId: createdPost.id,
+                    },
+                });
 
-            if (comment.replies) {
-                for (const reply of comment.replies) {
-                    await prisma.reply.create({
-                        data: {
-                            content: reply.content,
-                            userId: reply.userId,
-                            commentId: createdComment.id,
-                        },
-                    });
+                if (comment.replies) {
+                    for (const reply of comment.replies) {
+                        await prisma.reply.create({
+                            data: {
+                                content: reply.content,
+                                userId: reply.userId,
+                                commentId: createdComment.id,
+                            },
+                        });
+                    }
                 }
             }
-        }
 
-        await Promise.all(
-            post.reactions.map((reaction) =>
-                prisma.reaction.create({
-                    data: {
-                        userId: reaction.userId,
-                        postId: createdPost.id,
-                        type: reaction.type,
-                    },
-                }),
-            ),
-        );
+            await Promise.all(
+                post.reactions.map((reaction) =>
+                    prisma.reaction.create({
+                        data: {
+                            userId: reaction.userId,
+                            postId: createdPost.id,
+                            type: reaction.type,
+                        },
+                    }),
+                ),
+            );
+        }
     }
 
     console.log("✅ Seeding complete!");
