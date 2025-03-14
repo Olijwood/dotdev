69a0dc0c7dbe1571e4941936c4622dcb5c4f9d9e
olijwood <olijwood@gmail.com>
2025-03-14 17:01:26 +0000

    fix migration

--- prisma/migrations/20250313222927_extra_tag_fields/migration.sql
+++ prisma/migrations/20250313222927_extra_tag_fields/migration.sql
@@ -2,4 +2,3 @@
 ALTER TABLE "Tag" ADD COLUMN     "about" TEXT DEFAULT '',
 ADD COLUMN     "aboutLink" TEXT DEFAULT '',
 ADD COLUMN     "guidelines" TEXT DEFAULT '';
-ADD COLUMN     "displayName" TEXT DEFAULT '';
